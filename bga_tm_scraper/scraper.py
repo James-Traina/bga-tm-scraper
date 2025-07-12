be641ef7cb51bc68c41f556ba9a6b8e662660c11
James-Traina <31974698+James-Traina@users.noreply.github.com>
2025-07-11 20:28:30 -0400

    Remove unused expected_conditions import from scraper.py
    The expected_conditions (EC) import from Selenium is not used in the code

--- bga_tm_scraper/scraper.py
+++ bga_tm_scraper/scraper.py
@@ -12,7 +12,6 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 from bs4 import BeautifulSoup
 from datetime import datetime, timedelta
 from .bga_session import BGASession
